ccbb0cd091c2b697cd68aa9e48bd17147adfb807
John Hawthorn <john.hawthorn@gmail.com>
2016-08-06 01:31:11 -0700

    Create Player component

---
+++ app/components/Player.js
@@ -0,0 +1,11 @@
+import { h, Component } from 'preact'
+
+class Player extends Component {
+  render() {
+    return (
+      <div>Hello, world.</div>
+    );
+  }
+}
+
+export default Player
--- app/entry.js
+++ app/entry.js
@@ -1,10 +1,11 @@
 import "./style.scss";
+import Player from "./components/Player"
 
 import { h, render } from 'preact'
 
 const player = document.getElementById("player");
 
 render(
-    <div>Hello, world.</div>,
+    <Player />,
     player
     );
